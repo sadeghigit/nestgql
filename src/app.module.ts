b8c92444311b9a9a283aa30d85c200c123708174
Hamed <sadeghigit@gmail.com>
2022-12-03 09:32:01 +0330

    install mongodb module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE),
+  ],
 })
 export class AppModule {}
