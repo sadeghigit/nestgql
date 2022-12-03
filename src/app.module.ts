868f946966ef8ab11601ac5678198985ddcba94a
Hamed <sadeghigit@gmail.com>
2022-12-03 09:34:42 +0330

    install graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
   ],
 })
 export class AppModule {}
